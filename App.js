5a5a8c15c7aeb1e26cbec90ca2ed6948b0abdb3d
Devansh Walia <devansh.walia@thoughtworks.com>
2025-04-09 22:03:06 +0530

    remove image bg

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ImageBackground } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
 import Card from './components/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={{ uri: 'https://picsum.photos/1200/2400' }}
-        style={styles.backgroundImage}
-        resizeMode="cover"
-      >
+      
         <LinearGradient
           colors={['rgba(76, 102, 159, 0.9)', 'rgba(59, 89, 152, 0.9)', 'rgba(25, 47, 106, 0.9)']}
           style={styles.gradient}
@@ -19,7 +14,7 @@ export default function App() {
           <StatusBar style="light" />
           <Card />
         </LinearGradient>
-      </ImageBackground>
+      
     </View>
   );
 }
@@ -27,10 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
   },
   gradient: {
     flex: 1,
